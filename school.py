94ee262c0f4ff40730b8a6c59081ff01d4a4781f
Ahmed Safdar <asafdar1138@gmail.com>
2019-08-29 15:59:16 -0500

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
 class School:
+	def __init__(self, name=None, roster={}):
+		self.name = name
+		self.roster = roster
+
+	def add_student(self, name, grade):
+		if grade not in self.roster.keys():
+			self.roster[grade] = [name]
+		else:
+			self.roster[grade].append(name)
+
+	def grade(self, grade):
+		return self.roster[grade]
+
+	def sort_roster(self):
+		for key in self.roster:
+			self.roster[key].sort()
+		return self.roster
